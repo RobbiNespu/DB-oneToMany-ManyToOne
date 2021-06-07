fe54ab452c242d24364bcabd9bf9f65ac5abff5c
Robbi Nespu <robbinespu@gmail.com>
2021-06-08 01:30:43 +0800

    Unidirectional with @JoinColumn annotation

--- src/main/java/io/robbinespu/demo/model/Customer.java
+++ src/main/java/io/robbinespu/demo/model/Customer.java
@@ -8,6 +8,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -26,6 +27,7 @@ public class Customer implements Serializable {
             cascade = CascadeType.ALL,
             orphanRemoval = true
         )
+        @JoinColumn(name = "order")
         private List<Cart> cart = new ArrayList<>();
 
     
